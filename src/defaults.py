77b99e88f9804b65da63c3fea0683da0d163d9a9
unapproachable <tt_zed@hotmail.com>
2022-09-06 14:19:14 -0400

    Added defaults.py

---
+++ src/defaults.py
@@ -0,0 +1,10 @@
+DEFAULT_DIR_FORMAT = ['%Y', '%m', '%d']
+DEFAULT_NO_DATE_DIRECTORY = "unknown"
+DEFAULT_CONFIG_FILE_PATH = 'phockup.yaml'
+PROGRAM_DESCRIPTION = """\
+Media sorting tool to organize photos and videos from your camera in folders by year, \
+month and day.
+The software will collect all files from the input directory and copy them to the output
+directory without changing the files content. It will only rename the files and  place
+them in the proper directory for year, month and day.
+"""
